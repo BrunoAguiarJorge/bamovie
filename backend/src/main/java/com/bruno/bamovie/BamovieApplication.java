353ef15adec6e07a0584b312c50bdc939b81b316
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2022-05-11 19:36:59 +0100

    Project created

---
+++ backend/src/main/java/com/bruno/bamovie/BamovieApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.bamovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BamovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BamovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bruno/bamovie/BamovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.bamovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BamovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
