87c1bcba8a8f7ce1311ea67a088a5c4cb8fcc8be
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2022-05-12 16:37:14 +0100

    Domain model, database seed

---
+++ backend/src/main/java/com/bruno/bamovie/entities/Score.java
@@ -0,0 +1,43 @@
+package com.bruno.bamovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+	@EmbeddedId
+	private ScorePK id = new ScorePK();
+	
+	private Double value;
+	
+	public Score () {}
+	
+	//set the movie to the score
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	//set the User to the score 
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+}
