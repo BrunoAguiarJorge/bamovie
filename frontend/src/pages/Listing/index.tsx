0375974537f9a7c3cb217a051a38f9405a3d6be7
Bruno Aguiar Jorge <Bruno.a.jorge@gmail.com>
2022-05-15 22:17:38 +0100

    userState, userEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,13 +2,24 @@ import Pagination from "components/Pagination";
 import MovieCard from "components/MovieCard";
 import { BASE_URL } from "utils/requests";
 import axios from "axios";
+import { useState, useEffect } from "react";
+// import { MoviePage } from "types/movie";
 
 
 function Listing() {
-axios.get(`${BASE_URL}/movies?size=12&page=0`)
-.then(response => {
-    console.log(response.data);
-});
+
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=0`)
+            .then(response => {
+               // const data = response.data as MoviePage;
+               // setPageNumber(data.number);
+                console.log(response.data);
+            });
+    }, []);
+
+
 
     return (
         <>
@@ -31,7 +42,7 @@ axios.get(`${BASE_URL}/movies?size=12&page=0`)
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
-                  
+
                 </div>
             </div>
         </>
